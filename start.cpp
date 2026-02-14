6a8c742b60d4d21280d438a7d35693bbc9e0f599
Malhar150 <malharjardosh@gmail.com>
2026-02-15 04:46:00 +0530

    Add files via upload

---
+++ start.cpp
@@ -0,0 +1,78 @@
+// // how to write hello world in C++
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     cout << "Hello, World!" << endl;
+//     return 0;
+// }
+
+// // how to write addition of two numbers in C++
+// #include <iostream>
+// using namespace std;    
+// int main() {
+//     int num1, num2, sum;
+//     cout << "Enter two numbers: ";
+//     cin >> num1 >> num2;
+//     sum = num1 + num2;
+//     cout << "The sum is: " << sum << endl;
+//     return 0;
+// }
+
+// // how to print your name and age in C++
+// #include <iostream> 
+// using namespace std;
+// int main() {
+//     string name;
+//     int age;
+//     cout << "Enter your name: ";
+//     cin >> name;
+//     cout << "Enter your age: ";
+//     cin >> age;
+//     cout << "Hello, " << name << "! You are " << age << " years old." << endl;
+//     return 0;
+// }
+
+// how to swap two numbers in C++
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a, b, temp;
+//     cout << "Enter two numbers: ";     
+//     cin >> a >> b;       
+//     temp = a;
+//     a = b;
+//     b = temp;
+//     cout << "After swapping: " << endl;
+//     cout << "First number: " << a << endl;
+//     cout << "Second number: " << b << endl;
+//     return 0;
+// }
+
+// how to use for loop in c++
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter a number:";
+//     cin>> n;
+//     for(int i=1; i<=n; i++){
+//         cout<< i << endl;
+//     }
+//     return 0; 
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     for(i nt i=1;i<=10;i++){
+//         cout<< i <<"";
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
