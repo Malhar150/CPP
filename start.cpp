9d1dc04f5240d1a5cc0bf392f03aca9b6752f9e4
Malhar150 <malharjardosh@gmail.com>
2026-02-17 01:04:47 +0530

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -70,6 +70,24 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n, reverse = 0;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     while(n != 0) {
+//         reverse = reverse * 10 + (n % 10);
+//         n = n / 10;
+//     }
+
+//     cout << "Reversed number: " << reverse;
+//     return 0;
+// }
+
+
 
 
 
