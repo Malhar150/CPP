36731654ef10a76a9fcf1649d62b3f6b71de0783
Malhar150 <malharjardosh@gmail.com>
2026-03-10 19:00:22 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -102,3 +102,30 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "ENTER THE NUMBER: ";
+//     cin >> n;
+//     for(int i=0;i<=10;i++){
+//         cout<< n << "x"<< i << "=" << n*i << endl;
+//     }
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//    int n;
+//    long long factorial = 1;
+//    cout << "ENTER THE NUMBER: ";
+//    cin>> n;
+//    for(int i=1; i<=n; i++){
+//        factorial *= i;
+//     }
+//     cout << "THE FACTORIAL OF " << n << " IS " << factorial << endl;
+//     return 0;
+// }
+
+
