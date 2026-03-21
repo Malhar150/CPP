ed315755c54cbf868c83b2a4f92a724088ea8c36
Malhar Jardosh <malharjardosh@gmail.com>
2026-03-22 00:27:34 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -280,6 +280,22 @@
 //     return 0;
 // }
 
+//--------Square Pattern in C++-------
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter n: ";
+//     cin >> n;
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= n; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
 
 
 
