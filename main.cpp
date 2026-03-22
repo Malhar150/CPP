d1f6fb94f4ebbc86a434e1bab86f330b8c576799
Malhar Jardosh <malharjardosh@gmail.com>
2026-03-22 22:32:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -296,6 +296,37 @@
 //     return 0;
 // }
 
+//--------Right Angled Triangle Pattern with numbers in C++-------
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter n: ";
+//     cin >> n;
+//     for(int i = 1; i <= n; i++){
+//         for(int j = 1; j <= i; j++){
+//             cout << i << " ";       
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+//--------Reverse Right Angled Triangle Pattern with numbers in C++-------
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout << "Enter n: ";
+//     cin>>n;
+//     for(int i=n ; i>=1; i--){
+//         for(int j=1; j<=i; j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
 
 
 
